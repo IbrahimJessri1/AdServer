aabdd90af3569a51d91d3db84a17052d7e4a717f
IbrahimJessri <73992245+IbrahimJessri@users.noreply.github.com>
2022-08-12 19:34:09 +0300

    committed

--- config/db.py
+++ config/db.py
@@ -15,14 +15,9 @@ served_ad_collection = conn.AdServer.served_ad
 
 
 
-
 #gen.update_many(collection, {}, {"$set" : {"marketing_info.times_served" : 0} })
 
 
-
 #print(all_ads[0]["marketing_info"]["max_cpc"])
-
-
-
 
 
