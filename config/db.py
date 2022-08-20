4c1394032fe42df8c41f0e5d908dcec13bff4fc5
IbrahimJessri <73992245+IbrahimJessri@users.noreply.github.com>
2022-08-20 17:11:17 +0300

    committed

--- config/db.py
+++ config/db.py
@@ -14,6 +14,13 @@ role_permission_collection = conn.AdServer.role_permission
 served_ad_collection = conn.AdServer.served_ad
 
 
+# res = gen.get_many(advertisement_collection, {"adinfo.advertiser_username" : "testAdv"})
+# print(res)
+
+
+# advertisement_collection.delete_many({})
+# interactive_advertisement_collection.delete_many({})
+# served_ad_collection.delete_many({})
 
 #gen.update_many(collection, {}, {"$set" : {"marketing_info.times_served" : 0} })
 
